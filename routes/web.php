9e7af9d8d47354d61b351dd153948470ed009380
Salsa <salsaagustinanuriana@gmail.com>
2025-12-15 15:53:55 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tentang', function () {
+     return view('tentang');
+});
+
+
+Route::get('/sapa/{nama}', function ($nama) {
+    return "Halo!  selamat datang $nama di toko online";
+
+});
+
+Route::get('/sapa/{nama}', function ($nama='semua') {
+    return "Halo!  selamat datang $nama di toko online";
+
+});
+
+
+
+
